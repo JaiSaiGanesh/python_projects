65ba3e58c19fc19a10f0f069f1dab7e21ffd2c68
Your Name <youremailaddress@_Example_ .com>
2023-09-03 14:58:49 +0530

    created functions

--- basic_programs.py
+++ basic_programs.py
@@ -1,283 +1,215 @@
 
 
-#addition of two numbers
-first_number=int(input("enter a number: "))
-second_number=int(input("enter a number: "))
-result=first_number+second_number
-print("addition of two numbers is:",result)
+
+def addition_of_numbers(first_number,second_number):
+    result=first_number+second_number
+    return("addition of two numbers is:",result)
 
 
-#multiplication of two numbers
-first_number=int(input("enter a number: "))
-second_number=int(input("enter a number: "))
-result=first_number*second_number
-print("multiplication of two numbers is:",result)
+def multiplication_of_numbers(first_number,second_number):
+    result=first_number*second_number
+    return("multiplication of two numbers is:",result)
 
+def division_of_numbers(first_number,second_number):
+    def float_division():
+        result=first_number/second_number
+        return("float division of two numbers is:",result)
+    def floor_division():
+        result=(first_number//second_number)
+        return("Floor Division:", result)
 
-#divide two numbers
-first_number=int(input("enter a number: "))
-second_number=int(input("enter a number: "))
-result=first_number/second_number #float division
-print("Float Division:", result)
+def substraction_of_numbers(first_number,second_number):
+    result=first_number-second_number
+    return("substraction of two numbers is:",result)
 
-result=(first_number//second_number)#floor division
-print("Floor Division:", result)
+def swapping_with_third_variable(first_number,second_number):
+    third_variable = first_number   
+    first_number= second_number
+    second_number= third_variable
+    return(f"First number: {first_number} , Second Number:{second_number}")
 
+def swapping_without_third_variable(first_number,second_number):
+    result=first_number+second_number
+    first_number=result-first_number
+    second_number=result-second_number
+    return(f"First number: {first_number} , Second Number:{second_number}")
 
-#substraction of two numbers
-first_number=int(input("enter a number: "))
-second_number=int(input("enter a number: "))
-result=first_number-second_number
-print("substraction of two numbers is:",result)
+def area_of_a_triangle(base,height):
+    area =(base * height)/2   
+    return("Area of triangle :", round((area),2))
 
-
-#swaping of two numbers with third variable
-first_number=int(input("enter a number: "))
-second_number=int(input("enter a number: "))
-third_variable = first_number   
-first_number= second_number
-second_number= third_variable
-print(f"First number: {first_number} , Second Number:{second_number}")
-
-
-#swaping of two numbers without third variable
-first_number=int(input("enter a number: "))
-second_number=int(input("enter a number: "))
-result=first_number+second_number
-first_number=result-first_number
-second_number=result-second_number
-print(f"First number: {first_number} , Second Number:{second_number}")
-
-
-#squareroot of a number 
-num1=float(input("enter a number: "))
-root= num1**(1/2)
-print('Square root:',round(root,2))
-
-
-#calculate the area of traingle
-base=int (input ("Enter base:" ))
-height= int ( input ('enter height:' ) )
-area =(base * height)/2   
-print("Area of triangle :", round((area),2))
-
-
-
-
-#check if a number is postive or negative or 0
-numb=float(input("Enter any number"))
-if ((numb>0)):
-    print(f"{numb} is positive")
-elif ((numb<0)):
-    print( f"{numb}is negetive")
-else:
-    print(f"{numb} is 0")
-
-
-#check if a number is odd or even
-numb = float(input("Enter any number "))
-if (((numb%2)==0)):
-    print("{numb} is Even ")
-else:
-    print('{} is Odd'.format(numb))
-
-
-
-#find largest number among three numbers
-first_number=int(input("enter 1st number: "))
-second_number=int(input("enter second number: "))
-third_number=int(input("enter third number: "))
-largest=(max((first_number, second_number,third_number)))
-if first_number==second_number and second_number == third_number:
-    print("{} {} {}".format(first_number , second_number, third_number),"are equal" )
-else:
-    print("{}".format(largest,"is Largest Number"))
-
-
-#check whether a year is leap year or not
-year=int(input("enter the year"))
-if(((year %4==0)&(year%100!=0))|(year%400==0)):
-    print(f'{year} is Leap Year')
-else:
-    print(f'{year} is Not A LeapYear')
-    
-
-#check prime number
-numb=int(input('Enter Any Number: '))
-if numb==0|numb<0|numb==1:
-    print("its not a prime number: ")
-else:
-    for i in range(2,(numb//2+1)):
-        if numb%i==0:
-            print("it is not prime number")      
-    else:   
-        print (f'{numb} Is Prime Number')
-
-
-
-#check prime number in a interval
-inter1=int(input('Enter interval1 Number: '))
-inter2=int(input('Enter interval2 Number: '))
-
-for i in range(inter1,inter2):
-    if i==0|i<0|i==1:
-        print("its not a prime number: ")
+def positive_or_negative(numb):
+    if ((numb>0)):
+        return(f"{numb} is positive")
+    elif ((numb<0)):
+        return( f"{numb}is negetive")
     else:
-        for j in range(2,(i//2+1)):
-            if i%j==0:
-                break 
+        return(f"{numb} is 0")
+
+def even_or_odd(numb):
+    if (((numb%2)==0)):
+        return(f"{numb} is Even ")
+    else:
+        return('{} is Odd'.format(numb))
+
+def largest_number_among_three(first_number,second_number,third_number):
+    largest=(max((first_number, second_number,third_number)))
+    if first_number==second_number and second_number == third_number:
+        return("{} {} {}".format(first_number , second_number, third_number),"are equal" )
+    else:
+        return("{}".format(largest,"is Largest Number"))
+
+def leap_year(year):
+    if(((year %4==0)&(year%100!=0))|(year%400==0)):
+        return(f'{year} is Leap Year')
+    else:
+        return(f'{year} is Not A LeapYear ')
+
+
+def prime_number(numb):
+    if numb==0|numb<0|numb==1:
+        return("its not a prime number: ")
+    else:
+        for i in range(2,(numb//2+1)):
+            if numb%i==0:
+                return("it is not prime number")      
         else:   
-            print(f'{i} Is Prime Number')
+            return (f'{numb} Is Prime Number')
+
+def prime_number_in_interval(inter1,inter2):
+    for i in range(inter1,inter2):
+        if i==0|i<0|i==1:
+            return("its not a prime number: ")
+        else:
+            for j in range(2,(i//2+1)):
+                if i%j==0:
+                    break 
+            else:   
+                return(f'{i} Is Prime Number')
 
 
-
-#factorial of a number
-#method1
-number = int(input("enter the number")) 
-factorial=1
-while number>1 :
-    factorial+=factorial*(number-1)
-    number=number-1
-print(factorial)
-    
-#method2
-if number < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif number == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,number + 1):
-       factorial = factorial*i
-   print("The factorial of",number,"is",factorial)
-
-
-#fibbinocci sequence
-number=int(input("enter the last value of sequence: "))
-a=0
-b=1
-c=1
-while a<number:
-   print(a)
-   a=b
-   b=c
-   c=a+b
-   
-
-#check whether given number is armstrong number or not
-num=int(input("enter a number: "))
-sum_of_digit=0
-temp=num
-while temp>0:
-    reminder=temp%10
-    sum_of_digit+=(pow((reminder),3))
-    temp=temp//10
-
-if (sum_of_digit)==num:
-    print(num,' is armstrong number.' )    
-else:
-   print(num,"is not an Armstrong number")
-
-
-#hcf or gcd of a number
-
-
-x = int(input("enter a number: ")) 
-y = int(input("enter a number: "))
-hcf=0
-if x > y:
-        smaller = y
-else:
-    smaller = x
-for i in range(1, smaller+1):
-    if((x % i == 0) and (y % i == 0)):
-        hcf = i 
-print("The H.C.F. is", hcf)
-
-
-
-#lcm of a number
-x = int(input("enter a number: ")) 
-y = int(input("enter a number: "))
-hcf=0
-if x > y:
-        smaller = y
-else:
-    smaller = x
-for i in range(1, smaller+1):
-    if((x % i == 0) and (y % i == 0)):
-        hcf = i 
-
-print("LCM=", ((x*y)//hcf))
-
-
-#check the string is palindrome or not?
-str1="malayalam" #palindrome
-str2=""
-length=len(str1)
-for i in range(length-1,-1,-1):
-   str2+= str1[i]
-if str2==str1:
-   print(str1,"it is palindrome")
-   
-
-
-#add two matrices
-# Program to add two matrices using nested loop
-
-X = [[12,7,3],
-    [4 ,5,6],
-    [7 ,8,9]]
-
-Y = [[5,8,1],
-    [6,7,3],
-    [4,5,9]]
-
-result = [[0,0,0],
-         [0,0,0],
-         [0,0,0]]
-
-# iterate through rows
-for i in range(len(X)):
-   # iterate through columns
-   for j in range(len(X[0])):
-       result[i][j] = X[i][j] + Y[i][j]
-
-for r in result:
-   print(r)
-
-
-# Program to multiply two matrices using nested loops
-# 3x3 matrix
-X = [[12,7,3],
-    [4 ,5,6],
-    [7 ,8,9]]
-# 3x4 matrix
-Y = [[5,8,1,2],
-    [6,7,3,0],
-    [4,5,9,1]]
-# result is 3x4
-result = [[0,0,0,0],
-         [0,0,0,0],
-         [0,0,0,0]]
-
-# iterate through rows of X
-for i in range(len(X)):
-   # iterate through columns of Y
-   for j in range(len(Y[0])):
-       # iterate through rows of Y
-       for k in range(len(Y)):
-           result[i][j] += X[i][k] * Y[k][j]
-
-for r in result:
-   print(r)
-
-# number of occurences of a item in string
-string1="i am a good boy. do you know that ? yes of course i know that so what ? "
-x=string1.split(" ")
-dict={}
-for i in x:
-    if i in dict:
-        dict[i]+=1
+def factorial_of_a_number(number):
+    if number < 0:
+        return("Sorry, factorial does not exist for negative numbers")
+    elif number == 0:
+        return("The factorial of 0 is 1")
     else:
-        dict[i]=1
-print(dict)
+        for i in range(1,number + 1):
+            factorial = factorial*i
+    return("The factorial of",number,"is",factorial)
+
+def fibbinocci_sequence(number):
+    a=0
+    b=1
+    c=1
+    while a<number:
+        return(a)
+        a=b
+        b=c
+        c=a+b
+    
+def armstrong_number(num):
+    sum_of_digit=0
+    temp=num
+    while temp>0:
+        reminder=temp%10
+        sum_of_digit+=(pow((reminder),3))
+        temp=temp//10
+
+    if (sum_of_digit)==num:
+        return(num,' is armstrong number.' )    
+    else:
+        return(num,"is not an Armstrong number")
+
+def hcf(x,y):
+    
+    hcf=0
+    if x > y:
+            smaller = y
+    else:
+        smaller = x
+    for i in range(1, smaller+1):
+        if((x % i == 0) and (y % i == 0)):
+            hcf = i 
+    return("The H.C.F. is", hcf)
+
+def lcm(x,y):
+    
+    hcf=0
+    if x > y:
+            smaller = y
+    else:
+        smaller = x
+    for i in range(1, smaller+1):
+        if((x % i == 0) and (y % i == 0)):
+            hcf = i 
+
+    return("LCM=", ((x*y)//hcf))
+
+def palindrom(str1):
+    
+    str2=""
+    length=len(str1)
+    for i in range(length-1,-1,-1):
+        str2+= str1[i]
+    if str2==str1:
+        return(str1,"it is palindrome")
+
+def addition_of_matrix():
+    X = [[12,7,3],
+        [4 ,5,6],
+        [7 ,8,9]]
+    Y = [[5,8,1],
+        [6,7,3],
+        [4,5,9]]
+
+    result = [[0,0,0],
+            [0,0,0],
+            [0,0,0]]
+
+        # iterate through rows
+    for i in range(len(X)):
+        # iterate through columns
+        for j in range(len(X[0])):
+            result[i][j] = X[i][j] + Y[i][j]
+
+    for r in result:
+        return(r)
+
+
+def multiplication_of_matix():
+
+
+    X = [[12,7,3],
+        [4 ,5,6],
+        [7 ,8,9]]
+    # 3x4 matrix
+    Y = [[5,8,1,2],
+        [6,7,3,0],
+        [4,5,9,1]]
+    # result is 3x4
+    result = [[0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0]]
+
+    # iterate through rows of X
+    for i in range(len(X)):
+    # iterate through columns of Y
+        for j in range(len(Y[0])):
+        # iterate through rows of Y
+            for k in range(len(Y)):
+                result[i][j] += X[i][k] * Y[k][j]
+
+    for r in result:
+        return(r)
+
+def no_of_occurence_of_an_item (string1):
+    x=string1.split(" ")
+    dict={}
+    for i in x:
+        if i in dict:
+            dict[i]+=1
+        else:
+            dict[i]=1
+    return(dict)
+
